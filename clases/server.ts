3799359a54bd1917abeb76f04c51e9d1d2c6799c
GuillermoMafla <cjativa12@hotmail.com>
2019-11-19 22:53:13 -0500

    singleton

--- clases/server.ts
+++ clases/server.ts
@@ -1,20 +1,43 @@
 import { SERVER_PORT } from './../global/enviroment';
 import express from 'express';
+import socketIO from 'socket.io';
+import http from 'http';
 //SERVER_PORT
 
 
 export default class Server{
 
+    public static _instance:Server;
+
     public app:express.Application;
     public port: number;
+    public io:socketIO.Server;
+    private httpServer: http.Server
 
-    constructor(){
+    private constructor(){
         this.app=express();
         this.port=SERVER_PORT;
+        this.httpServer=new http.Server(this.app)
+        this.io=socketIO(this.httpServer)
+
+        this.escucharSockets();
+    }
+
+    public static get instance(){
+        return this._instance || (this._instance=new this());
+    }
+
+
+    private escucharSockets(){
+        console.log("Escuchando conexiones");
+
+        this.io.on("connection",cliente=>{
+            console.log("Cliente conectado")
+        })
     }
 
 
     start(callback:any){
-        this.app.listen(this.port , callback);
+        this.httpServer.listen(this.port , callback);
     }
 }
